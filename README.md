8b75a1b2d74109108ca8469d152704a5dd64a13d
zhazijie <473172339@qq.com>
2024-02-15 18:42:13 +0800

    frontend dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 docker build -t freight .  
 
+cd frontend 
+docker build -t freight-frontend .
+
--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -19,7 +19,7 @@ def _init_sqlalchemy(app, config):
 
 
 def get_connection_url(config):
-    password = os.environ.get('MY_ENV_PASSWORD') or ":" + config["PASSWORD"] if len(config["PASSWORD"]) > 0 else ""
+    password = ":" + os.environ.get('MY_ENV_PASSWORD') or ":" + config["PASSWORD"] if len(config["PASSWORD"]) > 0 else ""
     user = os.environ.get('MY_ENV_USER') or config["USER"]
     host = os.environ.get('MY_ENV_HOST') or str(config["HOST"]) + ":" + str(config["PORT"])
 
